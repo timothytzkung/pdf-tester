7937e1dfdd654418c4c3f816eb27abd33950e23a
Tim Kung <timothytzkung@gmail.com>
2026-02-02 14:42:19 -0800

    Add/Fix: Added readme, removed script.js
    Update .gitignore

--- script.js
+++
@@ -1,6 +0,0 @@
-let http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-  console.log("Server Live!")
-}).listen(8080);
